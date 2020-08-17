749c827f9ecc8a0402fe21d896382e9d3aa88396
BAROKA <birotori@gmail.com>
2020-08-17 11:40:12 +0100

    log in funtion config

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,12 @@ class SessionsController < ApplicationController
   end
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = 'ooh ! something is wrong'
+
+    end
   end
 end
