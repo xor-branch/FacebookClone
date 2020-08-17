ba8cb97d48279daed73d6058ecf26653c175f89a
BAROKA <birotori@gmail.com>
2020-08-17 11:22:53 +0100

    Sign in funtion config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'sessions/new'
   resources :blogs
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
--- db/migrate/20200816110118_create_blogs.rb
+++
@@ -1,9 +0,0 @@
-class CreateBlogs < ActiveRecord::Migration[5.2]
-  def change
-    create_table :blogs do |t|
-      t.text :content
-      t.text :image
-      t.timestamps
-    end
-  end
-end
